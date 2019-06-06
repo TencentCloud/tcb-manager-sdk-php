10a3b338a91be76e5b9dace2e67c39c53eae4bdb
liuyanjie <lyj8888888888@gmail.com>
2019-06-06 20:32:18 +0800

    feature(ci): supprot travis-ci

--- tests/TestBase.php
+++ tests/TestBase.php
@@ -4,13 +4,14 @@
 namespace TcbManager\Tests;
 
 
+use TcbManager\Constants;
 use TcbManager\TcbManager;
 
 class TestBase
 {
     public static $secretId;
     public static $secretKey;
-    public static $secretToken = "";
+    public static $secretToken;
     public static $envId;
 
     /**
@@ -20,11 +21,22 @@ class TestBase
 
     public static function init()
     {
-        self::$secretId = Config::$secretId;
-        self::$secretKey = Config::$secretKey;
-        self::$secretToken = Config::$secretToken;
+        if (getenv("CI", true) == "true"
+            && getenv("TRAVIS", true) == "true") {
+            // CI 环境
+            self::$secretId = getenv(Constants::ENV_SECRETID, true);
+            self::$secretKey = getenv(Constants::ENV_SECRETKEY, true);
+            self::$secretToken = getenv(Constants::ENV_SESSIONTOKEN, true);
+            self::$envId = getenv(Constants::ENV_TCB_ENV_ID, true);
+        }
+        else {
+            // 本地环境
+            self::$secretId = Config::$secretId;
+            self::$secretKey = Config::$secretKey;
+            self::$secretToken = Config::$secretToken;
+            self::$envId = Config::$envId;
+        }
 
-        self::$envId = Config::$envId;
         static::$tcb = TcbManager::init([
             "secretId" => Config::$secretId,
             "secretKey" => Config::$secretKey,
