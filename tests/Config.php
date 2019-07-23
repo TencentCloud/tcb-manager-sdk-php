86c1c4e508aa35d066506f0c7f8758c94208f9d3
liuyanjie <lyj8888888888@gmail.com>
2019-07-23 13:45:19 +0800

    Release 1.2.1

--- tests/Config.php
+++ tests/Config.php
@@ -3,8 +3,8 @@
 namespace TcbManager\Tests;
 
 class Config {
-    public static $secretId = "AKIDORiMgDgJrLPjxvKDm9F77NGixduHpm0o";
-    public static $secretKey = "JcFUANtb3cLtlbMuwZN9y6MIyMVXBPDG";
+    public static $secretId = "";
+    public static $secretKey = "";
     public static $secretToken = "";
 
     public static $envId = "demo-619e0a";
